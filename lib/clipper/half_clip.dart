380620b7782402df670990f43846069a411b8a1d
Andreas Huber <info@ndhbr.de>
2021-05-18 22:20:51 +0200

    UI

--- lib/clipper/half_clip.dart
+++ lib/clipper/half_clip.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
 
 class HalfClip extends CustomClipper<Rect> {
+  final bool left;
+
+  HalfClip({this.left});
+
   @override
   Rect getClip(Size size) {
-    Rect rect = Rect.fromLTRB(0.0, 0.0, size.width, size.height);
+    Rect rect;
+
+    if (left != null && left) {
+      rect = Rect.fromLTRB((size.width / 2 - 5), 0.0, size.width, size.height);
+    } else {
+      rect = Rect.fromLTRB(0.0, 0.0, (size.width / 2 + 5), size.height);
+    }
+
     return rect;
   }
 
   @override
   bool shouldReclip(HalfClip oldClipper) {
-    return true;
+    return false;
   }
 }
--- lib/pages/recording.dart
+++ lib/pages/recording.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:bikeangle/bikeangle.dart';
 import 'package:bikeangle/device_rotation.dart';
 import 'package:bikeangletest/clipper/half_clip.dart';
@@ -10,19 +13,30 @@ class Recording extends StatefulWidget {
   _RecordingState createState() => _RecordingState();
 }
 
-class _RecordingState extends State<Recording> {
+class _RecordingState extends State<Recording> with TickerProviderStateMixin {
   /// Bike Angle Library
   BikeAngle _bikeAngle;
+  StreamSubscription _rotationStream;
+  DeviceRotation _deviceRotation;
+
+  /// Rotation controller
+  AnimationController _rotationController;
 
   @override
   void initState() {
-    _bikeAngle = BikeAngle();
-
+    _init();
     super.initState();
   }
 
   @override
+  void dispose() {
+    _reset();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+
     return Container(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -35,86 +49,162 @@ class _RecordingState extends State<Recording> {
     );
   }
 
+  Future<void> _init() async {
+    _bikeAngle = BikeAngle();
+
+    Stream<DeviceRotation> stream = await _bikeAngle.startAngleRecording();
+    _rotationStream = stream.listen((deviceRotation) {
+      if (deviceRotation != null) {
+        _deviceRotation = deviceRotation;
+
+        if (mounted) {
+          _rotationController.animateTo(_deviceRotation.pitch.abs() / 90);
+        }
+
+        setState(() {});
+      }
+    });
+
+    _rotationController = AnimationController(
+      duration: const Duration(milliseconds: 100),
+      vsync: this,
+    );
+  }
+
+  Future<void> _reset() async {
+    await _bikeAngle.stopAngleRecording();
+
+    if (_rotationStream != null) {
+      await _rotationStream.cancel();
+      _rotationStream = null;
+    }
+  }
+
   Widget _buildAngleVisualization() {
+    double pitch = _deviceRotation?.pitch ?? 0.0;
+
+    if (pitch > 80) {
+      pitch = 80;
+    } else if (pitch < -80) {
+      pitch = -80;
+    }
+
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Row(
-        children: [
-          Flexible(
-            flex: 3,
-            child: ClipRRect(
-              clipper: HalfClip(),
-              child: SleekCircularSlider(
-                appearance: CircularSliderAppearance(
-                  customWidths: CustomSliderWidths(
-                    progressBarWidth: 24,
-                    handlerSize: 12,
-                    trackWidth: 24,
+      child: Container(
+        width: double.infinity,
+        child: Stack(
+          children: [
+            Positioned(
+              left: 48.0,
+              bottom: 0.0,
+              child: ClipRect(
+                clipper: HalfClip(),
+                child: SleekCircularSlider(
+                  appearance: CircularSliderAppearance(
+                    customWidths: CustomSliderWidths(
+                      progressBarWidth: 24,
+                      handlerSize: 6,
+                      trackWidth: 24,
+                    ),
+                    customColors: CustomSliderColors(
+                      shadowMaxOpacity: 0.0,
+                      trackGradientStartAngle: 260,
+                      trackGradientEndAngle: 340,
+                      trackColors: [
+                        Colors.green,
+                        Colors.yellow,
+                        Colors.red,
+                      ],
+                      progressBarColor: Colors.black26,
+                    ),
+                    angleRange: 80,
+                    startAngle: 260,
+                    counterClockwise: true,
                   ),
-                  angleRange: 80,
-                  startAngle: 260,
-                  counterClockwise: true,
+                  min: 0,
+                  max: 80,
+                  initialValue: (pitch < 0) ? pitch.abs() : 0,
+                  innerWidget: (_) => Container(),
                 ),
-                min: 0,
-                max: 80,
-                initialValue: 0,
-                innerWidget: (_) => Container(),
               ),
             ),
-          ),
-          Flexible(flex: 3, child: _buildAnimatingBike()),
-          Flexible(
-            flex: 3,
-            child: SleekCircularSlider(
-              appearance: CircularSliderAppearance(
-                customWidths: CustomSliderWidths(
-                  progressBarWidth: 24,
-                  handlerSize: 12,
-                  trackWidth: 24,
+            Positioned(
+              right: 48.0,
+              bottom: 0.0,
+              child: ClipRect(
+                clipper: HalfClip(left: true),
+                child: SleekCircularSlider(
+                  appearance: CircularSliderAppearance(
+                    customWidths: CustomSliderWidths(
+                      progressBarWidth: 24,
+                      handlerSize: 6,
+                      trackWidth: 24,
+                    ),
+                    customColors: CustomSliderColors(
+                      shadowMaxOpacity: 0.0,
+                      trackGradientStartAngle: 280,
+                      trackGradientEndAngle: 370,
+                      trackColors: [
+                        Colors.green,
+                        Colors.yellow,
+                        Colors.red,
+                      ],
+                      progressBarColor: Colors.black26,
+                    ),
+                    angleRange: 80,
+                    startAngle: 280,
+                  ),
+                  min: 0,
+                  max: 80,
+                  initialValue: (pitch > 0) ? pitch : 0,
+                  innerWidget: (_) => Container(),
                 ),
-                angleRange: 80,
-                startAngle: 280,
               ),
-              min: 0,
-              max: 80,
-              initialValue: 0,
-              innerWidget: (_) => Container(),
             ),
-          ),
-        ],
+            Center(
+              child: SizedBox(
+                width: 128,
+                child: _buildAnimatingBike(),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 
-  StreamBuilder<DeviceRotation> _buildAnimatingBike() {
-    return StreamBuilder<DeviceRotation>(
-      stream: _bikeAngle.listenToDeviceRotationEvents(),
-      builder: (context, snapshot) {
-        if (snapshot.hasError || !snapshot.hasData) {
-          return Text('Error');
-        }
+  Widget _buildAnimatingBike() {
+    if (_deviceRotation == null) {
+      return Text('Gyroscope nicht verfügbar');
+    }
 
-        DeviceRotation deviceRotation = snapshot.data;
+    // _deviceRotation.pitch / 360
 
-        return Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              height: MediaQuery.of(context).size.height * 0.1,
-              child: RotationTransition(
-                turns: new AlwaysStoppedAnimation(deviceRotation.pitch / 360),
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          height: MediaQuery.of(context).size.height * 0.20,
+          child: AnimatedBuilder(
+            animation: _rotationController,
+            builder: (context, child) {
+              return Transform.rotate(
+                angle: (_deviceRotation.pitch.sign == -1)
+                    ? -_rotationController.value
+                    : _rotationController.value,
                 alignment: Alignment.bottomCenter,
                 child: SvgPicture.asset('assets/motorcycle.svg'),
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            Text(
-              'Rotation ${deviceRotation.pitch.toStringAsFixed(0)} °',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-          ],
-        );
-      },
+              );
+            },
+          ),
+        ),
+        const SizedBox(height: 16.0),
+        Text(
+          'Rotation ${_deviceRotation.pitch.abs().toStringAsFixed(0)} °',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ],
     );
   }
 
